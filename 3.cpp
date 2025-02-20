610303fe31e41dc526a64a0f77b29c4f247046a8
Deeksha <deekshagowda817@gmail.com>
2025-02-20 22:39:59 +0530

    add

---
+++ 3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+   /**int a=9;
+    if(a==9){
+        cout<<"NINE";
+    }
+    if 
+    (a>0){
+        cout<<"POSITIVE";
+    }
+    else
+    {
+        cout<<"NEGATIVE";
+    }
+}*/
+
+{
+    int a=24;
+
+    if(a>20){
+        cout<<"Love";
+    }
+     else if(a==24) {
+        cout<<"Lovely";
+     }
+     else
+     {
+        cout<<"Babbar";
+     }
+    cout<<a;
+}
